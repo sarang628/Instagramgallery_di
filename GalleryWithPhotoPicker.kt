f29a3e1eba510a87bf244a368b1f877a13b38d9e
sryang <sarang628@gmail.com>
2025-08-04 12:43:51 +0800

    feat(gallery): update to use permission-free photo picker

--- GalleryWithPhotoPicker.kt
+++ GalleryWithPhotoPicker.kt
@@ -14,10 +14,10 @@ import androidx.compose.runtime.setValue
 import com.sarang.instagralleryModule.compose.GalleryListWithPreviewScreen
 
 @Composable
-fun GalleryWithPhotoPicker(){
+fun GalleryWithPhotoPicker(onNext: (List<String>) -> Unit = {}, onClose: () -> Unit = {}){
     var uris : List<Uri> by remember { mutableStateOf(listOf()) }
     val pickMultipleMedia =
-        rememberLauncherForActivityResult(PickMultipleVisualMedia(5)) { it ->
+        rememberLauncherForActivityResult(PickMultipleVisualMedia()) { it ->
             if (it.isNotEmpty()) {
                 Log.d("__PhotoPicker", "Number of items selected: ${it.size}")
                 uris = it
@@ -25,6 +25,8 @@ fun GalleryWithPhotoPicker(){
             else { Log.d("__PhotoPicker", "No media selected") }
         }
     GalleryListWithPreviewScreen(
+        onNext = onNext,
+        onClose = onClose,
         isPhotoPickerMode = true,
         list = uris.map { it.toString() },
         onPhotoPicker = { pickMultipleMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly)) })
